6398a3859f7e22995dba786ce2accae1dccd3501
Isabelle Oliveira <foliveira.isa@gmail.com>
2021-03-16 20:44:13 -0300

    Cria template e rota para cadastro de receita e cadastro de ingrediente

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -7,3 +7,11 @@ from app import app
 def index(user):
     return render_template('index.html', 
                             user=user)
+
+@app.route('/cadastro/ingrediente')
+def cadastro_ingrediente():
+    return render_template('cadastro_ingredientes.html')                            
+
+@app.route('/cadastro/receita')
+def cadastro_receita():
+    return render_template('cadastro_receitas.html')
